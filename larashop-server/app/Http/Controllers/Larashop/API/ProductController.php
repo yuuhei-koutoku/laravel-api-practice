b66e251ad749bbc16672b9d30c177cd00bd93b2b
yuuhei-koutoku <yhkt.0627@gmail.com>
2026-05-03 14:24:41 +0900

    06-03

--- larashop-server/app/Http/Controllers/Larashop/API/ProductController.php
+++ larashop-server/app/Http/Controllers/Larashop/API/ProductController.php
@@ -82,8 +82,8 @@ class ProductController extends Controller
      * @param  ShowRequest  $request
      * @return ProductResource
      */
-    // public function show(ShowRequest $request, Product $product)
-    // {
-    //     return new ProductResource($product);
-    // }
+    public function show(ShowRequest $request, Product $product)
+    {
+        return new ProductResource($product);
+    }
 }
--- larashop-server/routes/larashop_api.php
+++ larashop-server/routes/larashop_api.php
@@ -23,7 +23,7 @@ Route::post('/auth/signup/verify', [AuthController::class, 'signupVerify']);
 Route::post('/auth/signin', [AuthController::class, 'signin']);
 
 Route::get('/products', [ProductController::class, 'index']);
-// Route::get('/products/{product}', [ProductController::class, 'show']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
 
 Route::middleware(['auth:larashop_api', 'verified'])->group(function () {
     Route::post('/auth/signout', [AuthController::class, 'signout']);
